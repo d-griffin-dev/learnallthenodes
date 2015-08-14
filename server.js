d9dfa450026b20079629e5c097b521619c58589c
Daniel Griffin <dangriff50@gmail.com>
2015-08-14 12:57:37 -0400

    Video 3.1 adds some route handling

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
-var http = require('http');
+var http = require('http'), 
+    handler = function(req, res) {
+        if (req.url === '/'){
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Hello World\n');
+        } else if (req.url === '/marketing') {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('This is the marketing page.\n');
+        } else if (req.url === '/uploads') {
+            if (req.method === 'POST') {
+                console.log('Handling an upload');
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end('Handling the upload\n');
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end('<html><head><title>Uploads</title></head><body><form action="/uploads" method="POST">' + 
+                        '<input type="file" name="the_file"><input type="submit" value="Upload">' + 
+                        '</form></body></html>\n');
+            }
+        } else {
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Page not found\n');
+        }
+    },
+    server = http.createServer(handler);
 
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(1337);
+server.listen(1337);
 
 console.log('Server running at http://0.0.0.0:1337');
